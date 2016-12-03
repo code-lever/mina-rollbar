05f7abe14e1ea4c8b460f8b7d0d1c0fbafa81473
Evgrafov Denis <stereodenis@gmail.com>
2016-12-04 00:30:25 +0300

    Update to mina 1.0.x syntax

--- lib/mina/rollbar/tasks.rb
+++ lib/mina/rollbar/tasks.rb
@@ -27,60 +27,60 @@ require 'mina/rails'
 
 # ### rollbar_access_token
 # Sets the access token for your Rollbar account.  Required.
-set_default :rollbar_access_token, nil
+set :rollbar_access_token, nil
 
 # ### rollbar_username
 # Sets the Rollbar username of the user who deployed.  Optional.
-set_default :rollbar_username, nil
+set :rollbar_username, nil
 
 # ### rollbar_local_username
 # Sets the name of the user who deployed.  Defaults to `whoami`.  Optional.
-set_default :rollbar_local_username, %x[whoami].strip rescue nil
+set :rollbar_local_username, %x[whoami].strip rescue nil
 
 # ### rollbar_comment
 # Sets a deployment comment (what was deployed, etc.).  Optional.
-set_default :rollbar_comment, nil
+set :rollbar_comment, nil
 
 # ### rollbar_notification_debug
 # If true, enables verbosity in the notification to help debug issues.  Defaults to false.
-set_default :rollbar_notification_debug, false
+set :rollbar_notification_debug, false
 
 # ### :rollbar_environment
 # Sets the rollbar environment being deployed.  If left un-set, will default to `rails_env` value.
-set_default :rollbar_environment, nil
+set :rollbar_environment, nil
 
 namespace :rollbar do
 
   desc 'Notifies Rollbar of your deployment'
   task notify: :environment do
 
-    unless rollbar_access_token
+    unless fetch(:rollbar_access_token)
       print_error 'Must set your `:rollbar_access_token` to notify'
       exit
     end
 
-    unless branch? || commit?
+    unless set?(:branch) || set?(:commit)
       print_error 'Must define either `:branch` or `:commit`'
       exit
     end
 
-    revision = commit? ? commit : %x[git rev-parse #{branch}].strip
+    revision = set?(:commit) ? fetch(:commit) : %x[git rev-parse #{fetch(:branch)}].strip
 
-    silent = rollbar_notification_debug ? '-v' : '-s -o /dev/null'
+    silent = fetch(:rollbar_notification_debug) ? '-v' : '-s -o /dev/null'
     script = ["curl #{silent} https://api.rollbar.com/api/1/deploy/"]
-    script << "-F access_token=#{rollbar_access_token}"
-    if rollbar_environment
-      script << "-F environment=#{rollbar_environment}"
+    script << "-F access_token=#{fetch(:rollbar_access_token)}"
+    if set?(:rollbar_environment)
+      script << "-F environment=#{fetch(:rollbar_environment)}"
     else
-      script << "-F environment=#{rails_env}"
+      script << "-F environment=#{fetch(:rails_env)}"
     end
     script << "-F revision=#{revision}"
-    script << "-F local_username=#{rollbar_local_username}" if rollbar_local_username
-    script << "-F rollbar_username=#{rollbar_username}" if rollbar_username
-    script << "-F comment=#{rollbar_comment}" if rollbar_comment
+    script << "-F local_username=#{fetch(:rollbar_local_username)}" if set?(:rollbar_local_username)
+    script << "-F rollbar_username=#{fetch(:rollbar_username)}" if set?(:rollbar_username)
+    script << "-F comment=#{set(:rollbar_comment)}" if set?(:rollbar_comment)
 
-    queue! 'echo "-----> Notifying Rollbar of deployment"'
-    queue! %[#{script.join(' ')}]
+    comment %{Notifying Rollbar of deployment}
+    command %[#{script.join(' ')}]
 
   end
 
